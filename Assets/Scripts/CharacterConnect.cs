2bc12e9b8631058406f4eee091a9c097ee1bfaae
nusyeon <cocoa7445@gmail.com>
2023-03-17 21:33:37 +0900

    같은 종류의 캐릭터끼리 서버에 들어가면 따로 움직임
    수정해야 할 오류
    1. 먼저 들어간 쪽만 나중에 들어간 쪽을 볼 수 있음
    2. 나중에 들어간 쪽만 대화가 가능

---
+++ Assets/Scripts/CharacterConnect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using GameCreator.Runtime.Characters.Animim;
+using GameCreator.Runtime.Common;
+using GameCreator.Runtime.Characters;
+
+public class CharacterConnect : MonoBehaviourPun
+{
+    public Character Char;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Char = GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine == false)
+        {
+            Char.enabled = false;
+        }else if (photonView.IsMine == true)
+        {
+            Char.enabled = true;
+        }
+    }
+}
