30081a04b8cb730ca3a2974c724cbeb391a62244
Nayeon Ryu <2014027@sookmyung.ac.kr>
2023-08-06 12:27:29 +0900

    [feat] CharacterScene 교체

---
+++ Assets/Scripts/CharacterSelectManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterSelectManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+}
